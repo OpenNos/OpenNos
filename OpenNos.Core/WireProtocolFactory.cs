d56aa9ab63cc4543a5efc3f9951bd1a96a571571
ChuckTheRipper <chucktheripper@users.noreply.github.com>
2015-10-17 01:54:11 +0200

    implemented framing fixed #7

--- OpenNos.Core/WireProtocolFactory.cs
+++ OpenNos.Core/WireProtocolFactory.cs
@@ -5,9 +5,16 @@ namespace OpenNos.Core
     public class WireProtocolFactory<EncryptorT> : IScsWireProtocolFactory
         where EncryptorT : EncryptionBase
     {
+        private bool _useFraming;
+
+        public WireProtocolFactory(bool useFraming)
+        {
+            _useFraming = useFraming;
+        }
+
         public IScsWireProtocol CreateWireProtocol()
         {
-            return new WireProtocol<EncryptorT>();
+            return new WireProtocol<EncryptorT>(255, _useFraming);
         }
     }
 }
