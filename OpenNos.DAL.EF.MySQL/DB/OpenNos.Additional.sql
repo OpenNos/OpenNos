8d9b874af8df1ac2d0dd1fdca9abef23ae766cc9
ChuckTheRipper <chucktheripper@users.noreply.github.com>
2015-10-17 10:11:46 +0200

    added additional SQL, account.Name maxed to 255
    -  added unique constraint to account.Name (ucName)

---
+++ OpenNos.DAL.EF.MySQL/DB/OpenNos.Additional.sql
@@ -0,0 +1,10 @@
+﻿-- This File contains all of the additions to the OpenNos.edmx
+USE opennos;
+
+-- Unique constraints
+ALTER TABLE account
+ADD CONSTRAINT ucName UNIQUE (Name);
+
+-- Test Data
+INSERT INTO `opennos`.`account` (`AccountId`,`Name`,`Password`,`LastIp`,`Authority`,`LastSession`,`LastConnect`,`LoggedIn`)
+VALUES (1, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','127.0.0.1',2,0,'2015-01-01 00:00:00',0);
