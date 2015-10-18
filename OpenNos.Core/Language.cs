d664005c72e04267e85d1e617f14bfbb8ecbf019
0Lucifer0 <erwan.joly@hotmail.fr>
2015-10-19 01:18:45 +0200

    add multi language support {#11}

---
+++ OpenNos.Core/Language.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+using System.Threading;
+namespace OpenNos.Core
+{
+    public class Language
+    {
+        public Language()
+        {
+            CultureInfo newCultureInfo = new System.Globalization.CultureInfo(System.Configuration.ConfigurationManager.AppSettings["language"]);
+            Thread.CurrentThread.CurrentCulture = newCultureInfo;
+            Thread.CurrentThread.CurrentUICulture = newCultureInfo;
+        }
+        public string i18n(string message)
+        {
+            
+            ResourceManager resourceManager = new ResourceManager("OpenNos.Core.Resource.LocalizedResources", Assembly.GetExecutingAssembly());
+            if (resourceManager.GetString(message) != null && resourceManager.GetString(message) != "")
+                return resourceManager.GetString(message);
+            else
+                return String.Format("#{{0}}", message);
+        }
+    }
+}
