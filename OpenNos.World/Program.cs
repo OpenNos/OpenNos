05883582020c9e2fa655fdda85d91fcb19943d69
ChuckTheRipper <chucktheripper@users.noreply.github.com>
2015-10-19 20:34:41 +0200

    fixed logging for world server, added header

--- OpenNos.World/Program.cs
+++ OpenNos.World/Program.cs
@@ -6,12 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace OpenNos.World
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //define handers for received packets
             IList<Type> handlers = new List<Type>();
@@ -19,6 +20,12 @@ namespace OpenNos.World
 
             //initialize Logger
             Logger.InitializeLogger(LogManager.GetLogger(typeof(Program)));
+
+            Console.Title = "OpenNos World Server v1.0.0";
+            Console.WriteLine("===============================================================================\n"
+                             + "                 WORLD SERVER VERSION 1.0.0 by OpenNos Team\n" +
+                             "===============================================================================\n");
+
             NetworkManager<WorldEncryption> networkManager = new NetworkManager<WorldEncryption>("127.0.0.1", 1337, handlers, true);
         }
     }
