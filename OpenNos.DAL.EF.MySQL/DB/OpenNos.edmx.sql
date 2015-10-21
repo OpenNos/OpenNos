d5596e67852d51f0914be13a3ac01f806fb2eaed
ChuckTheRipper <chucktheripper@users.noreply.github.com>
2015-10-21 16:34:15 +0200

    changed Account - Warehouse association
    - currently not necessary and blocking

--- OpenNos.DAL.EF.MySQL/DB/OpenNos.edmx.sql
+++ OpenNos.DAL.EF.MySQL/DB/OpenNos.edmx.sql
@@ -44,7 +44,7 @@
 -- -----------------------------------------------------------
 -- Entity Designer DDL Script for MySQL Server 4.1 and higher
 -- -----------------------------------------------------------
--- Date Created: 10/20/2015 22:34:58
+-- Date Created: 10/21/2015 16:33:22
 
 -- Generated from EDMX file: C:\Users\Alex\Documents\GitHub\OpenNos\OpenNos.DAL.EF.MySQL\DB\OpenNos.edmx
 -- Target version: 3.0.0.0
