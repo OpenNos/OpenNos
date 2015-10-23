6a9c54fa34a7d12be27ca3134a90bb7c7667475f
ChuckTheRipper <chucktheripper@users.noreply.github.com>
2015-10-23 18:02:05 +0200

    cleanup, added unique constraint for AccountId & Slot

--- OpenNos.DAL.EF.MySQL/DB/OpenNos.Additional.sql
+++ OpenNos.DAL.EF.MySQL/DB/OpenNos.Additional.sql
@@ -5,6 +5,9 @@ USE opennos;
 ALTER TABLE account
 ADD CONSTRAINT ucName UNIQUE (Name);
 
+ALTER TABLE `character`
+ADD CONSTRAINT ucAccountIdSlot UNIQUE(AccountId,Slot);
+
 -- Test Data
 INSERT INTO `opennos`.`account` (`AccountId`,`Name`,`Password`,`Authority`,`LastSession`,`LoggedIn`)
 VALUES (1, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',2,0,0);
