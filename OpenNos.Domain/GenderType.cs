767c60abdd096471b6742834111f0b6d75c5d8bb
0Lucifer0 <erwan.joly@hotmail.fr>
2015-11-10 11:20:23 +0100

    fix issue 62 and 63

---
+++ OpenNos.Domain/GenderType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.Domain
+{
+    public enum GenderType : byte
+    {
+        Male = 0,
+        Female = 1
+    }
+}
---
+++ OpenNos.Domain/HairStyleType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.Domain
+{
+    public enum HairStyleType : byte
+    {
+        HairTypeA = 0,
+        HairTypeB = 1
+    }
+}
