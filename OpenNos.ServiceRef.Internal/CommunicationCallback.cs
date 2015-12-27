5c8efc19b0fc1505c51cc8760f29f8819a600216
ChuckTheRipper <chucktheripper@users.noreply.github.com>
2015-12-27 21:31:56 +0100

    implemented WCF Service
    fixed #80

---
+++ OpenNos.ServiceRef.Internal/CommunicationCallback.cs
@@ -0,0 +1,24 @@
+﻿using OpenNos.ServiceRef.Internal.CommunicationServiceReference;
+using OpenNos.WCF.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.ServiceRef.Internal
+{
+    public class CommunicationCallback : ICommunicationServiceCallback, IDisposable
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void RegisterPlayerLoginCallback(string value)
+        {
+            //communication callback will be useful for chatting (register a entity to received messages by callback)
+            //todo inform client
+        }
+    }
+}
