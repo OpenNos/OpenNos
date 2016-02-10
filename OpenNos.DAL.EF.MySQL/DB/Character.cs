41fc0b75e22d4208cf3fb4f0d5399386a3f48dc5
Dominik <dominik895@hotmail.com>
2016-02-10 23:20:23 +0100

    fix launguage bugs and start of config menu

--- OpenNos.DAL.EF.MySQL/DB/Character.cs
+++ OpenNos.DAL.EF.MySQL/DB/Character.cs
@@ -58,5 +58,6 @@ namespace OpenNos.DAL.EF.MySQL.DB
         public virtual Map map { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Inventory> inventory { get; set; }
+        public virtual Config Config { get; set; }
     }
 }
---
+++ OpenNos.Data/ConfigDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.Data
+{
+    public class ConfigDTO
+    {
+        public long ConfigId { get; set; }
+
+        public bool FriendRequestBlocked { get; set; }
+
+        public bool WhisperBlocked { get; set; }
+
+        public bool GroupRequestBlocked { get; set; }
+
+        public bool MouseAimLock { get; set; }
+
+        public bool HeroChatBlocked { get; set; }
+
+        public bool QuickGetUp { get; set; }
+
+        public bool EmoticonsBlocked { get; set; }
+
+        public bool HpBlocked { get; set; }
+
+        public bool BuffBlocked { get; set; }
+
+        public bool MinilandInviteBlocked { get; set; }
+    }
+}
