f0f2bb7c9d442afabfb2cc339a770588011b549e
Dominik <dominik895@hotmail.com>
2016-02-11 12:34:35 +0100

    Add ConfigType

---
+++ OpenNos.Domain/ConfigType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.Domain
+{
+    public enum ConfigType : byte
+    {
+        ExchangeBlocked = 1,
+        FriendRequestBlocked = 2,
+        WhisperBlocked = 4,
+        GroupRequestBlocked = 5,
+        MouseAimLock = 9,
+        HeroChatBlocked = 10,
+        EmoticonsBlocked = 11,
+        QuickGetUp = 12,
+        HpBlocked = 13,
+        BuffBlocked = 14,
+        MinilandInviteBlocked = 15
+    }
+}
