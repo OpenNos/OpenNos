20e273a01b227d259de0daccaa8cdc7a3d9e75fa
genyx <gx@genyx.eu>
2016-02-15 16:57:20 +0100

    Revert "#220"
    This reverts commit 4ba2e69992e2486645850fc065c5a02d3f3496b9.

--- OpenNos.Core/SessionFactory.cs
+++ OpenNos.Core/SessionFactory.cs
@@ -1,34 +1,36 @@
-﻿namespace OpenNos.Core
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.Core
 {
     public class SessionFactory
     {
         #region Members
 
-        private static SessionFactory _instance;
         private int _sessionCounter;
-
+        
         #endregion
 
-        #region Instantiation
+        #region Singleton
 
-        private SessionFactory()
-        {
-        }
+        private static SessionFactory _instance;
 
-        #endregion
-
-        #region Properties
+        private SessionFactory() { }
 
         public static SessionFactory Instance => _instance ?? (_instance = new SessionFactory());
 
         #endregion
+
 
         #region Methods
 
         public int GenerateSessionId()
         {
             _sessionCounter += 2;
-            return _sessionCounter;
+            return _sessionCounter; 
         }
 
         #endregion
