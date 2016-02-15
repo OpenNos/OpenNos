e25d583ad55689525a604bc4edb7259796ecd9ee
genyx <gx@genyx.eu>
2016-02-15 16:56:30 +0100

    #220

--- OpenNos.Core/SessionFactory.cs
+++ OpenNos.Core/SessionFactory.cs
@@ -1,36 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OpenNos.Core
+﻿namespace OpenNos.Core
 {
     public class SessionFactory
     {
         #region Members
 
+        private static SessionFactory _instance;
         private int _sessionCounter;
-        
+
         #endregion
 
-        #region Singleton
+        #region Instantiation
 
-        private static SessionFactory _instance;
+        private SessionFactory()
+        {
+        }
 
-        private SessionFactory() { }
+        #endregion
+
+        #region Properties
 
         public static SessionFactory Instance => _instance ?? (_instance = new SessionFactory());
 
         #endregion
-
 
         #region Methods
 
         public int GenerateSessionId()
         {
             _sessionCounter += 2;
-            return _sessionCounter; 
+            return _sessionCounter;
         }
 
         #endregion
