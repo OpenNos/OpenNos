423ca9f23c94181805fa0376bf58424875f85289
MasterDomino <MrDominoGrm@gmail.com>
2016-02-27 22:20:15 +0100

    add template

--- OpenNos.DAL.EF.MySQL/DB/OpenNos.edmx.sql
+++ OpenNos.DAL.EF.MySQL/DB/OpenNos.edmx.sql
@@ -44,9 +44,9 @@
 -- -----------------------------------------------------------
 -- Entity Designer DDL Script for MySQL Server 4.1 and higher
 -- -----------------------------------------------------------
--- Date Created: 02/27/2016 19:20:59
+-- Date Created: 02/27/2016 22:09:51
 
--- Generated from EDMX file: C:\Users\ERWAN\Desktop\OpenNos GIT\OpenNos.DAL.EF.MySQL\DB\OpenNos.edmx
+-- Generated from EDMX file: C:\Users\Dominik\Source\Repos\OpenNos\OpenNos.DAL.EF.MySQL\DB\OpenNos.edmx
 -- Target version: 3.0.0.0
 
 -- --------------------------------------------------
