efc6d7b16188d4397bb6ce97a7024c6ee1efe521
0Lucifer0 <erwan.joly@hotmail.fr>
2016-03-16 23:24:00 +0100

    fix a bug

---
+++ OpenNos.GameObject/Npc/NpcMonster.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * This file is part of the OpenNos Emulator Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+using AutoMapper;
+using OpenNos.DAL;
+using OpenNos.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace OpenNos.GameObject
+{
+    public class NpcMonster : NpcMonsterDTO
+    {
+        #region Instantiation
+
+        public short firstX
+        {
+            get; set;
+        }
+        public short firstY
+        {
+            get; set;
+        }
+        public NpcMonster()
+        {
+            Mapper.CreateMap<NpcMonsterDTO, NpcMonster>();
+            Mapper.CreateMap<NpcMonster, NpcMonsterDTO>();
+
+        }
+        public NpcMonster(short npcId)
+        {
+            Mapper.CreateMap<NpcMonsterDTO, NpcMonster>();
+            Mapper.CreateMap<NpcMonster, NpcMonsterDTO>();
+
+            LastEffect = LastMove = DateTime.Now;
+
+            IEnumerable<TeleporterDTO> Teleporters = DAOFactory.TeleporterDAO.LoadFromNpc(NpcMonsterVNum);
+         }
+
+        #endregion
+
+        #region Properties
+        public IEnumerable<TeleporterDTO> Teleporters { get; set; }
+        public DateTime LastMove { get; private set; }
+        public DateTime LastEffect { get; private set; }
+        #endregion
+
+        #region Methods
+        
+
+        #endregion
+    }
+}
