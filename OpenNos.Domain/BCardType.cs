d79568c19b2a1d00aa52f9747203fc5e8e11b825
MasterDomino <MrDominoGrm@gmail.com>
2016-03-23 14:46:32 +0100

    add BCardType

---
+++ OpenNos.Domain/BCardType.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * This file is part of the OpenNos Emulator Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+namespace OpenNos.Domain
+{
+    public class BCardType
+    {
+        #region Enums
+
+        public enum CardType : byte
+        {
+            AttackType = 1,
+            DefenceType = 2,
+            SelfType = 3,
+            TargetType = 4,
+        }
+    }
+    public class AdditionalTypes : BCardType
+    {
+        private enum AttackType : byte
+        {
+            AllIcreased = 11,
+            AllDecreased = 12,
+            MeleeIncreased = 21,
+            MeleeDecreased = 22,
+            RangedIncreased = 31,
+            RangedDecreased = 32,
+            MagicalIncreased = 41,
+            MagicalDecreased = 42,
+        }
+
+        private enum DefenceType : byte
+        {
+            AllIcreased = 11,
+            AllDecreased = 12,
+            MeleeIncreased = 21,
+            MeleeDecreased = 22,
+            RangedIncreased = 31,
+            RangedDecreased = 32,
+            MagicalIncreased = 41,
+            MagicalDecreased = 42,
+        }
+
+        private enum SelfType : byte
+        {
+            AllIcreased = 11,
+            AllDecreased = 12,
+            MeleeIncreased = 21,
+            MeleeDecreased = 22,
+            RangedIncreased = 31,
+            RangedDecreased = 32,
+            MagicalIncreased = 41,
+            MagicalDecreased = 42,
+        }
+
+        private enum TargetType : byte
+        {
+            AllIcreased = 11,
+            AllDecreased = 12,
+            MeleeIncreased = 21,
+            MeleeDecreased = 22,
+            RangedIncreased = 31,
+            RangedDecreased = 32,
+            MagicalIncreased = 41,
+            MagicalDecreased = 42,
+        }
+
+        #endregion
+
+    }
+}
