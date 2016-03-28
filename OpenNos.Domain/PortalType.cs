4882c4b67f3e58a7070a76a0bfb17de256ebf030
MasterDomino <MrDominoGrm@gmail.com>
2016-03-28 19:15:09 +0200

    add PortalType

---
+++ OpenNos.Domain/PortalType.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * This file is part of the OpenNos Emulator Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+namespace OpenNos.Domain
+{
+    public enum PortalType : sbyte
+    {
+        MapPortal = -1,
+        TSNormal = 0, // same over >127 - sbyte
+        Closed = 1,
+        Open = 2,
+        Miniland = 3,
+        TSEnd = 4,
+        TSEndClosed = 5,
+        End = 6,
+        EndClosed = 7,
+        Raid = 8,
+        Effect = 9, // same as 13-19 and 20 - 126
+        BlueRaidPortal = 10,
+        DarkRaidPortal = 11,
+        TimeSpace = 12,
+        ShopTeleport = 20,
+    }
+}
