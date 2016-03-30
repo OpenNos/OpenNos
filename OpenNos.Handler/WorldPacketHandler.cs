c9c4da03c0d0bb88d6037231b89ef4b53fe65646
MasterDomino <MrDominoGrm@gmail.com>
2016-03-30 19:04:18 +0200

    update Language resources

--- OpenNos.Handler/WorldPacketHandler.cs
+++ OpenNos.Handler/WorldPacketHandler.cs
@@ -706,7 +706,7 @@ namespace OpenNos.Handler
                         short Slot = inv.Slot;
                         if (Slot != -1)
                         {
-                            Session.Client.SendPacket(Session.Character.GenerateSay($"{Language.Instance.GetMessageFromKey("YOU_GET_OBJECT")}: {iteminfo.Name} x {amount}", 12));
+                            Session.Client.SendPacket(Session.Character.GenerateSay($"{Language.Instance.GetMessageFromKey("ITEM_ACQUIRED")}: {iteminfo.Name} x {amount}", 12));
                             Session.Client.SendPacket(Session.Character.GenerateInventoryAdd(vnum, inv.InventoryItem.Amount, iteminfo.Type, Slot, rare, design, upgrade));
                         }
                     }
