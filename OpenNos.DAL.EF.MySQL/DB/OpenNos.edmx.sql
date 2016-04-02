b22700f9aa65281723f1afb4beef995dbf07fa54
MasterDomino <MrDominoGrm@gmail.com>
2016-04-02 14:53:07 +0200

    add de and fr for import console

--- OpenNos.DAL.EF.MySQL/DB/OpenNos.edmx.sql
+++ OpenNos.DAL.EF.MySQL/DB/OpenNos.edmx.sql
@@ -44,9 +44,9 @@
 -- -----------------------------------------------------------
 -- Entity Designer DDL Script for MySQL Server 4.1 and higher
 -- -----------------------------------------------------------
--- Date Created: 04/02/2016 12:29:03
+-- Date Created: 04/02/2016 14:33:12
 
--- Generated from EDMX file: C:\Users\ERWAN\Desktop\OpenNos Git\OpenNos.DAL.EF.MySQL\DB\OpenNos.edmx
+-- Generated from EDMX file: C:\Users\Dominik\Source\Repos\OpenNos\OpenNos.DAL.EF.MySQL\DB\OpenNos.edmx
 -- Target version: 3.0.0.0
 
 -- --------------------------------------------------
