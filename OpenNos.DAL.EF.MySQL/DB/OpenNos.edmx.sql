a3b55445374bae3e8226659f8b95307e8cb889ae
MasterDomino <MrDominoGrm@gmail.com>
2016-04-04 21:10:51 +0200

    fix bad commit

--- OpenNos.DAL.EF.MySQL/DB/OpenNos.edmx.sql
+++ OpenNos.DAL.EF.MySQL/DB/OpenNos.edmx.sql
@@ -44,9 +44,9 @@
 -- -----------------------------------------------------------
 -- Entity Designer DDL Script for MySQL Server 4.1 and higher
 -- -----------------------------------------------------------
--- Date Created: 04/04/2016 19:13:01
+-- Date Created: 04/04/2016 21:05:11
 
--- Generated from EDMX file: C:\Users\ERWAN\Desktop\OpenNos Git\OpenNos.DAL.EF.MySQL\DB\OpenNos.edmx
+-- Generated from EDMX file: C:\Users\Dominik\Source\Repos\OpenNos\OpenNos.DAL.EF.MySQL\DB\OpenNos.edmx
 -- Target version: 3.0.0.0
 
 -- --------------------------------------------------
