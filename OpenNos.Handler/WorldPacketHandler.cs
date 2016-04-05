7992c1c68c73961e28a6fde6399f280241d97645
0Lucifer0 <erwan.joly@hotmail.fr>
2016-04-06 01:47:44 +0200

    add cast effet

--- OpenNos.Handler/WorldPacketHandler.cs
+++ OpenNos.Handler/WorldPacketHandler.cs
@@ -4164,22 +4164,8 @@ namespace OpenNos.Handler
                             int critical_chance = 10;
                             int miss_chance = 20;
                             int criticalhit = 0;
-                            int AtkType = 0;
-                            switch(Session.Character.Class)
-                            {
-                                case 0:
-                                    AtkType = 0;
-                                    break;
-                                case 1:
-                                    AtkType = 0;
-                                    break;
-                                case 2:
-                                    AtkType = 1;
-                                    break;
-                                case 3:
-                                    AtkType = 2;
-                                    break;
-                            }
+                            int AtkType = skill.Type;
+
                             switch (AtkType)
                             {
                                 case 0:
@@ -4297,7 +4283,7 @@ namespace OpenNos.Handler
                             mmon.Target = Session.Character.CharacterId;
 
 
-                           
+
                             Task cast = Task.Factory.StartNew(async () =>
                             {
                                 if (skill.CastEffect != 0)
@@ -4307,19 +4293,21 @@ namespace OpenNos.Handler
                                 }
                             });
 
+                         Task t=   Task.Factory.StartNew(async () =>
+                               {
+
+                                   await cast;
+
+                                   string packet = $"su {1} {Session.Character.CharacterId} {3} {mmon.MapMonsterId} {skill.Effect} 6 {skill.AttackAnimation} {skill.Effect} 0 0 {(mmon.Alive ? 1 : 0)} {(int)(((float)mmon.CurrentHp / (float)monsterinfo.MaxHP) * 100)} {damage} {hitmode} {skill.Type}";
+                                   ClientLinkManager.Instance.Broadcast(Session, packet, ReceiverType.AllOnMap);
+
+                               });
+
                             Task.Factory.StartNew(async () =>
-                             {
-
-                                 await cast;
-
-                                 string packet = $"su {1} {Session.Character.CharacterId} {3} {mmon.MapMonsterId} {skill.Effect} 6 {skill.AttackAnimation} {skill.Effect} 0 0 {(mmon.Alive ? 1 : 0)} {(int)(((float)mmon.CurrentHp / (float)monsterinfo.MaxHP) * 100)} {damage} {hitmode} {skill.Type}";
-                                 ClientLinkManager.Instance.Broadcast(Session, packet, ReceiverType.AllOnMap);
-
-                             });
-                            Task t = Task.Factory.StartNew(async () =>
                             {
+                                await t;
                                 ski.Used = true;
-                                await Task.Delay(skill.Cooldown * 100);
+                                await Task.Delay(skill.Cooldown * 100 - skill.CastTime * 100);
                                 Session.Client.SendPacket($"sr {Castingid}");
                                 ski.Used = false;
                             });
