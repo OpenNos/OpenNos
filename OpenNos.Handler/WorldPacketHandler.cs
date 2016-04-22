f1fc88c8eae17fa298dcd9e7ed686eec00ab4afe
0Lucifer0 <erwan.joly@hotmail.fr>
2016-04-23 00:13:15 +0200

    fix crypto

--- OpenNos.Handler/WorldPacketHandler.cs
+++ OpenNos.Handler/WorldPacketHandler.cs
@@ -2553,7 +2553,7 @@ namespace OpenNos.Handler
             int damage;
             int hitmode = 0;
             Skill skill = null;
-            CharacterSkill ski = skills.FirstOrDefault(s => (skill = ServerManager.GetSkill(s.SkillVNum)) != null && skill.CastId == Castingid);
+            CharacterSkill ski = skills.FirstOrDefault(s => (skill = ServerManager.GetSkill(s.SkillVNum)) != null && skill?.CastId == Castingid);
             if (!ski.Used)
             {
                 if (skill != null && skill.TargetType == 1 && skill.HitType == 1)
