cc0021e85f4d9c7e51b3f8c9836136ec3ea0ac1f
0Lucifer0 <erwan.joly@hotmail.fr>
2016-05-03 21:16:52 +0200

    fix sell skill bug fix #509

--- OpenNos.Handler/NpcPacketHandler.cs
+++ OpenNos.Handler/NpcPacketHandler.cs
@@ -459,7 +459,7 @@ namespace OpenNos.Handler
                 short.TryParse(packetsplit[4], out vnum);
                 Skill skillinfo = ServerManager.GetSkill(vnum);
                 CharacterSkill skill = Session.Character.Skills.FirstOrDefault(s => s.SkillVNum == vnum);
-                if (skill == null || skillinfo == null)
+                if (skill == null || skillinfo == null || skill == Session.Character.Skills.ElementAt(0) || skill == Session.Character.Skills.ElementAt(1))
                     return;
                 Session.Character.Gold -= skillinfo.Price;
                 Session.Client.SendPacket(Session.Character.GenerateGold());
