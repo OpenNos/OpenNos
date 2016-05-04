3fbc6c52fc346d9db130979823c9943e8bae309f
0Lucifer0 <erwan.joly@hotmail.fr>
2016-05-04 15:32:19 +0200

    fix last commit

--- OpenNos.DAL.EF.MySQL/Entities/MapNpc.cs
+++ OpenNos.DAL.EF.MySQL/Entities/MapNpc.cs
@@ -12,13 +12,11 @@ namespace OpenNos.DAL.EF.MySQL
             Recipe = new HashSet<Recipe>();
             Shop = new HashSet<Shop>();
             Teleporter = new HashSet<Teleporter>();
-            LifeTaskIsRunning = false;
         }
 
         #endregion
 
         #region Properties
-        public bool LifeTaskIsRunning { get; internal set; }
         public short Dialog { get; set; }
 
         public short Effect { get; set; }
