882e340d150f7035a2da2a1c77f3c9e82d43945d
chucktheripper <aleqs.schwab@gmail.com>
2016-05-10 22:21:07 +0200

    broadcasting speedup test

--- OpenNos.GameObject/Networking/BroadcastableBase.cs
+++ OpenNos.GameObject/Networking/BroadcastableBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Subjects;
 
 namespace OpenNos.GameObject
@@ -37,14 +38,52 @@ namespace OpenNos.GameObject
 
         public bool Broadcast(ClientSession client, string message, ReceiverType receiver = ReceiverType.All, string characterName = "", long characterId = -1)
         {
-            SessionPacket packet = new SessionPacket(client, message, receiver, characterName, characterId);
-            _subject.OnNext(packet);
+            switch (receiver)
+            {
+                case ReceiverType.All:
+                    for (int i = Sessions.Keys.Where(s => s != null && s.Character != null).Count() - 1; i >= 0; i--)
+                        Sessions.Keys.Where(s => s != null).ElementAt(i).Client.SendPacket(message);
+                    break;
+
+                case ReceiverType.AllExceptMe:
+                    for (int i = Sessions.Keys.Where(s => s != null && s.Character != null && s != client).Count() - 1; i >= 0; i--)
+                        Sessions.Keys.Where(s => s != null && s != client).ElementAt(i).Client.SendPacket(message);
+                    break;
+
+                case ReceiverType.OnlyMe:
+                    client.Client.SendPacket(message);
+                    break;
+
+                case ReceiverType.OnlySomeone:
+                    ClientSession targetSession = Sessions.Keys.FirstOrDefault(s => s.Character != null && (s.Character.Name.Equals(characterName) || s.Character.CharacterId.Equals(characterId)));
+
+                    if (targetSession == null) return false;
+
+                    targetSession.Client.SendPacket(message);
+                    return true;
+
+                case ReceiverType.AllNoEmoBlocked:
+                    foreach (ClientSession session in Sessions.Keys.Where(s => s.Character != null && s.Character.MapId.Equals(client.Character.MapId) && !s.Character.EmoticonsBlocked))
+                        session.Client.SendPacket(message);
+                    break;
+
+                case ReceiverType.AllNoHeroBlocked:
+                    foreach (ClientSession session in Sessions.Keys.Where(s => s.Character != null && !s.Character.HeroChatBlocked))
+                        session.Client.SendPacket(message);
+                    break;
+
+                case ReceiverType.Group:
+                    foreach (ClientSession session in Sessions.Keys.Where(s => s.Character != null && s.Character.Group != null && s.Character.Group.GroupId.Equals(client.Character.Group.GroupId)))
+                        session.Client.SendPacket(message);
+                    break;
+            }
+
             return true;
         }
 
         public virtual void RegisterSession(ClientSession session)
         {
-            if(!Sessions.ContainsKey(session))
+            if (!Sessions.ContainsKey(session))
             {
                 Sessions.Add(session, _subject.Subscribe(s => session.CallbackSessionRequest(s)));
             }
@@ -52,7 +91,7 @@ namespace OpenNos.GameObject
 
         public virtual void UnregisterSession(ClientSession session)
         {
-            if(Sessions.ContainsKey(session))
+            if (Sessions.ContainsKey(session))
             {
                 Sessions[session].Dispose();
                 Sessions.Remove(session);
