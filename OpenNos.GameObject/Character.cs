3775c4b821a568afb8b07bbf378a1449f37403dc
chucktheripper <aleqs.schwab@gmail.com>
2016-05-10 17:43:39 +0200

    fixed #570, loading of quicklist, -1 instead of 0 for empty quicklist item

--- OpenNos.GameObject/Character.cs
+++ OpenNos.GameObject/Character.cs
@@ -648,7 +648,7 @@ namespace OpenNos.GameObject
                 for (int j = 0; j < 3; j++)
                 {
                     QuicklistEntry qi = QuicklistEntries.FirstOrDefault(n => n.Q1 == j && n.Q2 == i && n.Morph == (UseSp ? Morph : 0));
-                    pktQs[j] += string.Format(" {0}.{1}.{2}", qi?.Type, qi?.Slot, qi != null ? qi.Pos : 0);
+                    pktQs[j] += string.Format(" {0}.{1}.{2}", qi?.Type != null ? qi?.Type : 7 , qi?.Slot != null ? qi?.Slot : 7, qi != null ? qi.Pos.ToString() : "-1");
                 }
             }
 
