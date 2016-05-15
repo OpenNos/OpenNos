51ce9bef15e45c7092b1112b4208ef3850b5e30d
KrisYiu <chrisyuki@yahoo.com.hk>
2016-05-15 18:32:34 +0800

    Fix bug
    fixed Quick entry disappear when auto learn skill.

--- OpenNos.Handler/BattlePacketHandler.cs
+++ OpenNos.Handler/BattlePacketHandler.cs
@@ -1,16 +1,16 @@
 ﻿/*
-* This file is part of the OpenNos Emulator Project. See AUTHORS file for Copyright information
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*/
+ * This file is part of the OpenNos Emulator Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 using OpenNos.Core;
 using OpenNos.Data;
@@ -80,7 +80,7 @@ namespace OpenNos.Handler
             List<CharacterSkill> skills = Session.Character.UseSp ? Session.Character.SkillsSp : Session.Character.Skills;
             bool notcancel = false;
 
-            if (skills != null)
+             if (skills != null)
             {
                 ushort damage = 0; ;
                 int hitmode = 0;
