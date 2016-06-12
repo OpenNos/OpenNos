a93f59c5782096d35182560acbdab08a3ca27a20
0Lucifer0 <erwan.joly@hotmail.fr>
2016-06-12 17:05:49 +0200

    fix basic pass

--- OpenNos.DAL.EF.MySQL/Migrations/Configuration.cs
+++ OpenNos.DAL.EF.MySQL/Migrations/Configuration.cs
@@ -30,8 +30,8 @@ namespace OpenNos.DAL.EF.MySQL.Migrations
         {
             IList<Account> accounts = new List<Account>();
 
-            accounts.Add(new Account() { AccountId = 1, Authority = Domain.AuthorityType.Admin, Name = "admin", Password = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08" });
-            accounts.Add(new Account() { AccountId = 2, Authority = Domain.AuthorityType.User, Name = "test", Password = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08" });
+            accounts.Add(new Account() { AccountId = 1, Authority = Domain.AuthorityType.Admin, Name = "admin", Password = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff" });
+            accounts.Add(new Account() { AccountId = 2, Authority = Domain.AuthorityType.User, Name = "test", Password = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff" });
 
             context.Account.AddOrUpdate(a => a.AccountId, accounts.ToArray());
         }
