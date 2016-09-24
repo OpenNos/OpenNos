6e083c907989d58061647784dbcfabd3a1f8be6a
0Lucifer0 <erwan.joly@hotmail.fr>
2016-09-24 16:11:15 +0200

    try fix right click

--- OpenNos.Handler/InventoryPacketHandler.cs
+++ OpenNos.Handler/InventoryPacketHandler.cs
@@ -164,8 +164,7 @@ namespace OpenNos.Handler
                     case 5:
                         if (Session.Character.ExchangeInfo != null)
                         {
-                            byte exchangeInventoryType;
-                            if (byte.TryParse(packetsplit[3], out exchangeInventoryType) && short.TryParse(packetsplit[4], out slot))
+                            if (short.TryParse(packetsplit[4], out slot))
                             {
                                 ExchangeInfo exch = ServerManager.Instance.GetProperty<ExchangeInfo>(Session.Character.ExchangeInfo.CharacterId, nameof(Character.ExchangeInfo));
                                 Guid id;
