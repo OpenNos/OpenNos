81ce8ff4c4307d39b7b6de97fa5de5d607bf3a50
Anthony boucher <anthony456@hotmail.be>
2016-10-06 18:27:53 +0200

    little fix

--- OpenNos.GameObject/Networking/ClientSession.cs
+++ OpenNos.GameObject/Networking/ClientSession.cs
@@ -550,7 +550,7 @@ namespace OpenNos.GameObject
 
             // ignore a packet which has been sent after the last one, in waiting to an better way to
             // prevent spam packets (Disable now because cause a lot of troubles)
-            ///if (IsAuthenticated && currentPacketReceive - lastPacketReceive < 120000 && !IsLocalhost)
+            //if (IsAuthenticated && currentPacketReceive - lastPacketReceive < 120000 && !IsLocalhost)
             //{
             //   Logger.Log.Warn($"[AntiSpam]: Packet has been ignored, access was too fast. Last: {lastPacketReceive}, Current: {currentPacketReceive}, Difference: {currentPacketReceive - lastPacketReceive}, SessionId: {SessionId}");
             // Disconnect();
