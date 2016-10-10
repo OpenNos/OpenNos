46d61809893001b79abcecd746b405671e1e3bc1
0Lucifer0 <erwan.joly@hotmail.fr>
2016-10-10 08:40:27 +0200

    try a fix

--- OpenNos.World/WorldEncryption.cs
+++ OpenNos.World/WorldEncryption.cs
@@ -241,7 +241,7 @@ namespace OpenNos.World
 
         public override byte[] Encrypt(string str)
         {
-            byte[] StrBytes = System.Text.Encoding.Convert(System.Text.Encoding.UTF8, System.Text.Encoding.Default, System.Text.Encoding.UTF8.GetBytes(str));
+            byte[] StrBytes = Encoding.Default.GetBytes(str);
             int BytesLength = StrBytes.Length;
 
             byte[] encryptedData = new byte[BytesLength + (int)Math.Ceiling((decimal)BytesLength / 0x7E) + 1];
