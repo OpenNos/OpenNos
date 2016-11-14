be8293cf1e1e131effb93f7e89fe3ce0143221ee
Martazza <Martazza>
2016-11-14 19:16:42 +0100

    Fix #1457, Fix #1346

--- OpenNos.Handler/NpcPacketHandler.cs
+++ OpenNos.Handler/NpcPacketHandler.cs
@@ -120,8 +120,8 @@ namespace OpenNos.Handler
                     {
                         // load shop
                         MapNpc npc = Session.CurrentMap.Npcs.FirstOrDefault(n => n.MapNpcId.Equals((short)buyPacket.OwnerId));
-
-                        if (npc == null || npc.Shop == null || Map.GetDistance(new MapCell() { MapId = Session.CurrentMap.MapId, X = Session.Character.MapX, Y = Session.Character.MapY }, new MapCell() { MapId = npc.MapId, X = npc.MapX, Y = npc.MapY }) > 5)
+                        int dist = Map.GetDistance(new MapCell() { MapId = Session.CurrentMap.MapId, X = Session.Character.MapX, Y = Session.Character.MapY }, new MapCell() { MapId = npc.MapId, X = npc.MapX, Y = npc.MapY });
+                        if (npc == null || npc.Shop == null || dist > 5)
                         {
                             return;
                         }
