5fa19aef173840d8f46f3edbd6e8ba61888e6fef
MasterDomino <MrDominoGrm@gmail.com>
2016-12-12 15:46:47 +0100

    add FamilyPacketHandler placeholder

---
+++ OpenNos.Handler/FamilyPacketHandler.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * This file is part of the OpenNos Emulator Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+using OpenNos.Core;
+using OpenNos.GameObject;
+
+namespace OpenNos.Handler
+{
+    public class FamilyPacketHandler : IPacketHandler
+    {
+        #region Members
+
+        private readonly ClientSession _session;
+
+        #endregion
+
+        #region Instantiation
+
+        public FamilyPacketHandler(ClientSession session)
+        {
+            _session = session;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ClientSession Session
+        {
+            get { return _session; }
+        }
+
+        #endregion
+
+        // there will be some mad methods
+    }
+}
