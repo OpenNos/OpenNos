aa1b43b69d5dea6ac80ce92bc9aa90d752d4527b
Ciapa <markus@ciapa.io>
2016-12-24 11:03:11 +0100

    Fix XP Rate

--- OpenNos.GameObject/Character.cs
+++ OpenNos.GameObject/Character.cs
@@ -3080,7 +3080,7 @@ namespace OpenNos.GameObject
 
             // bonus percentage calculation for level 1 - 5 and difference of levels bigger or equal
             // to 4
-            if(levelDifference <= 20)
+            if(levelDifference <= -20)
             {
                 xp /= 10;
             }
