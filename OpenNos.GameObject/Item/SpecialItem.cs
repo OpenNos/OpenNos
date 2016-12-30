6812d825737d243bd8f5cc22392f8ae929909d56
Ciapa <markus@ciapa.io>
2016-12-30 22:02:47 +0100

    Add more contents to sealed treasure chests

--- OpenNos.GameObject/Item/SpecialItem.cs
+++ OpenNos.GameObject/Item/SpecialItem.cs
@@ -167,20 +167,19 @@ namespace OpenNos.GameObject
                     if (EffectValue == 69)
                     {
                         int rnd = ServerManager.RandomNumber(0, 1000);
-                        if (rnd < 5)
+                        if (rnd < 15)
                         {
-                            session.Character.GiftAdd(1160, 10);
-                        }
-                        else if (rnd < 15)
-                        {
-                            short[] specialVnums = new short[] { 5560, 5591, 4099, 907 };
-                            session.Character.GiftAdd(specialVnums[ServerManager.RandomNumber(0,4)], 1);
+                            short[] vnums = new short[] { 5560, 5591, 4099, 907, 1160, 4705, 4706, 4707, 4708, 4709, 4710, 4711, 4712, 4713, 4714, 4715, 4716 };
+                            byte[] counts = new byte[] { 1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+                            int item = ServerManager.RandomNumber(0, 17);
+
+                            session.Character.GiftAdd(vnums[item], counts[item]);
                         }
                         else
                         {
-                            short[] vnums = new short[] { 1160, 2282, 1030, 1244, 1218, 5369, 1012, 1363, 1364, 2160, 2173, 5959, 5983, 2514, 2515, 2516, 2517, 2518, 2519, 2520, 2521, 1685, 1686, 5087, 5203 };
-                            byte[] counts = new byte[] { 1, 10, 20, 5, 1, 1, 99, 1, 1, 5, 5, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1 };
-                            int item = ServerManager.RandomNumber(0, 25);
+                            short[] vnums = new short[] { 1160, 2282, 1030, 1244, 1218, 5369, 1012, 1363, 1364, 2160, 2173, 5959, 5983, 2514, 2515, 2516, 2517, 2518, 2519, 2520, 2521, 1685, 1686, 5087, 5203, 2418, 2310, 2303, 361, 362, 363, 366, 367, 368, 371, 372, 373, 2169, 2280, 5892, 5893, 5894, 5895, 5896, 5897, 5898, 5899, 5332, 5105, 2161, 2162 };
+                            byte[] counts = new byte[] { 1, 10, 20, 5, 1, 1, 99, 1, 1, 5, 5, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 5, 20, 20, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+                            int item = ServerManager.RandomNumber(0, 51);
                             session.Character.GiftAdd(vnums[item], counts[item]);
                         }
                         session.Character.Inventory.RemoveItemAmountFromInventory(1, inv.Id);
