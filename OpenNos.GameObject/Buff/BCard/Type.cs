ea5531ac034bf6018adad3c9ea6025bbbf938468
Ciapa <markus@ciapa.io>
2017-01-15 13:27:00 +0100

    Implement Basic Buff System

---
+++ OpenNos.GameObject/Buff/BCard/Type.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.GameObject.Buff.BCard
+{
+    public enum Type
+    {
+        Damage,
+        Element,
+        HitRate,
+        Morale,
+        Defense,
+        Dodge,
+        Resistance,
+        HP,
+        MP,
+        Speed,
+        Effect,
+        Cooldown
+    }
+}
---
+++ OpenNos.GameObject/Buff/Indicators/SP1/Swordsman/Sprint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.GameObject.Buff.Indicators.SP1.Swordsman
+{
+    public class Sprint : IndicatorBase
+    {
+        public Sprint(int Level)
+        {
+            Name = "Sprint";
+            Duration = 100;
+            Id = 93;
+            DirectBuffs.Add(new BCardEntry(BCard.Type.Speed, BCard.SubType.Increase, 5, 0, false));
+        }
+    }
+}
