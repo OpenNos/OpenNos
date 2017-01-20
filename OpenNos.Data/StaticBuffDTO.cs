c18d05d13b1347f6fb046f6a8252fe09dc8efc8f
MasterDomino <MrDominoGrm@gmail.com>
2017-01-20 14:02:51 +0100

    add miniland db entries for character and staticbuff table

---
+++ OpenNos.Data/StaticBuffDTO.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * This file is part of the OpenNos Emulator Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+namespace OpenNos.Data
+{
+    public class StaticBuffDTO
+    {
+        #region Properties
+
+        public long CharacterId { get; set; }
+
+        public int EffectId { get; set; }
+
+        public int RemainingTime { get; set; }
+
+        public long StaticBuffId { get; set; }
+
+        #endregion
+    }
+}
