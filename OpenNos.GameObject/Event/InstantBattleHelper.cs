d13edd3f2a1bc697f30ad02616e4c93d31bbbbed
root <root@GAME-12236>
2017-01-21 12:11:56 +0100

    continue instant battle

--- OpenNos.GameObject/Event/InstantBattleHelper.cs
+++ OpenNos.GameObject/Event/InstantBattleHelper.cs
@@ -8,96 +8,75 @@ namespace OpenNos.GameObject.Event
 {
     public class InstantBattleHelper
     {
-        public static List<Tuple<short, short, short, long, bool>> GetLodMonster(Map map, short instantbattletype, int wave)
+        public static List<Tuple<short, short, short, long, bool>> GenerateMonsters(Map map, short vnum, short amount, bool move)
         {
             List<Tuple<short, short, short, long, bool>> SummonParameters = new List<Tuple<short, short, short, long, bool>>();
             MapCell cell;
+            for (int i = 0; i < 15; i++)
+            {
+                cell = map.GetRandomPosition();
+                SummonParameters.Add(new Tuple<short, short, short, long, bool>(vnum, cell.X, cell.Y, -1, true));
+            }
+            return SummonParameters;
+        }
+
+
+        public static List<Tuple<short, int, short, short>> GenerateDrop(Map map, short vnum, int amountofdrop, int amount)
+        {
+            List<Tuple<short, int, short, short>> dropParameters = new List<Tuple<short, int, short, short>>();
+            MapCell cell = null;
+            for (int i = 0; i < amountofdrop; i++)
+            {
+                cell = map.GetRandomPosition();
+                dropParameters.Add(new Tuple<short, int, short, short>(vnum, amount, cell.X, cell.Y));
+            }
+            return dropParameters;
+        }
+        public static List<Tuple<short, short, short, long, bool>> GetLodMonster(Map map, short instantbattletype, int wave)
+        {
+            List<Tuple<short, short, short, long, bool>> SummonParameters = new List<Tuple<short, short, short, long, bool>>();
+
             switch (instantbattletype)
             {
                 case 70:
                     switch (wave)
                     {
                         case 0:
-                            for (int i = 0; i < 15; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(402, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(253, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(237, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(216, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(205, cell.X, cell.Y, -1, true));
-                            }
-
+                            SummonParameters.AddRange(GenerateMonsters(map, 402, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 253, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 237, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 216, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 205, 15, true));
                             break;
                         case 1:
-                            for (int i = 0; i < 15; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(402, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(243, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(228, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(225, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(205, cell.X, cell.Y, -1, true));
-                            }
+                            SummonParameters.AddRange(GenerateMonsters(map, 402, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 243, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 228, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 225, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 205, 15, true));
                             break;
                         case 2:
-                            for (int i = 0; i < 15; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(255, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(254, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(251, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(174, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(172, cell.X, cell.Y, -1, true));
-                            }
+                            SummonParameters.AddRange(GenerateMonsters(map, 255, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 254, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 251, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 174, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 172, 15, true));
                             break;
                         case 3:
-                            for (int i = 0; i < 15; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(407, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(272, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(261, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(257, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(256, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(175, cell.X, cell.Y, -1, true));
-                            }
+                            SummonParameters.AddRange(GenerateMonsters(map, 407, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 272, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 261, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 257, 15, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 256, 15, true));
                             break;
                         case 4:
-                            cell = map.GetRandomPosition();
-                            SummonParameters.Add(new Tuple<short, short, short, long, bool>(748, cell.X, cell.Y, -1, true));
-                            for (int i = 0; i < 13; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(444, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(439, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(275, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(274, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(273, cell.X, cell.Y, -1, true));
-                                cell = map.GetRandomPosition();
-                                SummonParameters.Add(new Tuple<short, short, short, long, bool>(163, cell.X, cell.Y, -1, true));
-                            }
+                            SummonParameters.AddRange(GenerateMonsters(map, 748, 1, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 444, 13, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 439, 13, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 275, 13, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 274, 13, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 273, 13, true));
+                            SummonParameters.AddRange(GenerateMonsters(map, 163, 13, true));
                             break;
                     }
 
@@ -110,86 +89,32 @@ namespace OpenNos.GameObject.Event
         public static List<Tuple<short, int, short, short>> GetLodDrop(Map map, short instantbattletype, int wave)
         {
             List<Tuple<short, int, short, short>> dropParameters = new List<Tuple<short, int, short, short>>();
-            MapCell cell = null;
             switch (instantbattletype)
             {
                 case 70:
                     switch (wave)
                     {
                         case 0:
-                            for (int i = 0; i < 15; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1046,3000, cell.X, cell.Y));
-                            }
-                            for (int i = 0; i < 8; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1010, 3, cell.X, cell.Y));
-                            }
-                            for (int i = 0; i < 5; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1246, 1, cell.X, cell.Y));
-                            }
+                            dropParameters.AddRange(GenerateDrop(map, 1046, 15, 3000));
+                            dropParameters.AddRange(GenerateDrop(map, 1010, 8, 3));
+                            dropParameters.AddRange(GenerateDrop(map, 1246, 5, 1));
                             break;
                         case 1:
-                            for (int i = 0; i < 15; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1046, 4000, cell.X, cell.Y));
-                            }
-                            for (int i = 0; i < 10; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1010, 4, cell.X, cell.Y));
-                            }
-                            for (int i = 0; i < 10; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1247, 1, cell.X, cell.Y));
-                            }
+                            dropParameters.AddRange(GenerateDrop(map, 1046, 15, 4000));
+                            dropParameters.AddRange(GenerateDrop(map, 1010, 15, 4));
+                            dropParameters.AddRange(GenerateDrop(map, 1247, 10, 1));
                             break;
                         case 2:
-                            for (int i = 0; i < 15; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1046, 5000, cell.X, cell.Y));
-                            }
-                            for (int i = 0; i < 13; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1010, 5, cell.X, cell.Y));
-                            }
-                            for (int i = 0; i < 13; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1246, 1, cell.X, cell.Y));
-                            }
-                            for (int i = 0; i < 13; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1247, 1, cell.X, cell.Y));
-                            }
+                            dropParameters.AddRange(GenerateDrop(map, 1046, 15, 5000));
+                            dropParameters.AddRange(GenerateDrop(map, 1010, 13, 5));
+                            dropParameters.AddRange(GenerateDrop(map, 1246, 13, 1));
+                            dropParameters.AddRange(GenerateDrop(map, 1247, 13, 1));
                             break;
                         case 3:
-                            for (int i = 0; i < 15; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1046, 7000, cell.X, cell.Y));
-                            }
-                            for (int i = 0; i < 13; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1011, 5, cell.X, cell.Y));
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1248, 1, cell.X, cell.Y)); 
-                            }
-                            for (int i = 0; i < 5; i++)
-                            {
-                                cell = map.GetRandomPosition();
-                                dropParameters.Add(new Tuple<short, int, short, short>(1029, 1, cell.X, cell.Y));
-                            }
+                            dropParameters.AddRange(GenerateDrop(map, 1046, 15, 7000));
+                            dropParameters.AddRange(GenerateDrop(map, 1011, 13, 5));
+                            dropParameters.AddRange(GenerateDrop(map, 1248, 13, 1));
+                            dropParameters.AddRange(GenerateDrop(map, 1029, 5, 1));
                             break;
                     }
 
