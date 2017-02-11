c889e6e4579a24d127eb4437c98f14567d027ad6
Ciapa <markus@ciapa.io>
2017-02-11 12:55:14 +0100

    Fix Crash in #sl Packet

--- OpenNos.Handler/InventoryPacketHandler.cs
+++ OpenNos.Handler/InventoryPacketHandler.cs
@@ -827,7 +827,7 @@ namespace OpenNos.Handler
              Logger.Debug(Session.GenerateIdentity(), packet);
             string[] packetsplit = packet.Split(' ', '^');
             byte mode;
-            if (byte.TryParse(packetsplit[2], out mode) && !Session.Character.UseSp && !Session.Character.IsVehicled)
+            if (packetsplit.Length > 2 && byte.TryParse(packetsplit[2], out mode) && !Session.Character.UseSp && !Session.Character.IsVehicled)
             {
                 double currentRunningSeconds = (DateTime.Now - Process.GetCurrentProcess().StartTime.AddSeconds(-50)).TotalSeconds;
                 double timeSpanSinceLastSpUsage = currentRunningSeconds - Session.Character.LastSp;
