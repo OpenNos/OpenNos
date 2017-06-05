7c8327bfea70548f36c287a379b59e4b2f0967a0
0Lucifer0 <erwan.joly@gmail.com>
2017-06-05 12:59:00 +0200

    fix bcard 255

--- OpenNos.Import.Console/ImportFactory.cs
+++ OpenNos.Import.Console/ImportFactory.cs
@@ -3443,13 +3443,13 @@ namespace OpenNos.Import.Console
                     {
                         for (int i = 0; i < 5; i++)
                         {
-                            byte type = (byte)(Int32.Parse(currentLine[2 + 5 * i]));
-                            if (type != 0)
+                            int type = Int32.Parse(currentLine[2 + 5 * i]);
+                            if (type != 0 && type != -1)
                             {
                                 BCardDTO itemCard = new BCardDTO
                                 {
                                     ItemVNum = item.VNum,
-                                    Type = type,
+                                    Type = (byte)type,
                                     SubType = (byte)((int.Parse(currentLine[5 + 5 * i]) + 1)),
                                     FirstData = (short)(int.Parse(currentLine[3 + 5 * i]) / 4),
                                     SecondData = (short)(int.Parse(currentLine[4 + 5 * i]) / 4),
