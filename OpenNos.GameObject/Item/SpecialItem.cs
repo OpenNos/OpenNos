75a8a61e858c2f8c52da968ce830ac59eb73301e
Syl_ <syleze.contact@gmail.com>
2017-08-25 14:29:48 +0200

    Add: Mates are now removed from map when using vehicle also they respawn close to you when removing vehicle #30

--- OpenNos.GameObject/Item/SpecialItem.cs
+++ OpenNos.GameObject/Item/SpecialItem.cs
@@ -267,6 +267,7 @@ namespace OpenNos.GameObject
                                     session.Character.MorphUpgrade = 0;
                                     session.Character.MorphUpgrade2 = 0;
                                     session.Character.Morph = Morph + (byte)session.Character.Gender;
+                                    session.Character.Mates.ForEach(x => session.CurrentMapInstance?.Broadcast(x.GenerateOut()));
                                     session.CurrentMapInstance?.Broadcast(session.Character.GenerateEff(196), session.Character.MapX, session.Character.MapY);
                                     session.CurrentMapInstance?.Broadcast(session.Character.GenerateCMode());
                                     session.SendPacket(session.Character.GenerateCond());
@@ -275,6 +276,12 @@ namespace OpenNos.GameObject
                             }
                             else if (session.Character.IsVehicled)
                             {
+                                session.Character.Mates.ForEach(x =>
+                                {
+                                    x.PositionX = session.Character.PositionX;
+                                    x.PositionY = session.Character.PositionY;
+                                    session.CurrentMapInstance?.Broadcast(x.GenerateIn());
+                                });
                                 session.Character.RemoveVehicle();
                             }
                         }
