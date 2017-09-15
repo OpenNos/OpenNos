c7d9daafbc9a82629e58552445c936fc606b6ece
Syl_ <syleze.contact@gmail.com>
2017-09-15 21:14:03 +0200

    [ADD] psl packet

---
+++ OpenNos.GameObject/Packets/ClientPackets/PslPacket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenNos.Core;
+
+namespace OpenNos.GameObject.Packets.ClientPackets
+{
+    [PacketHeader("psl")]
+    public class PslPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public int Type { get; set; }
+    }
+}
