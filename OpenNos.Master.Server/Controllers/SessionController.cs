effa53c0d40e07892d8e81cb5d7b80f0a8406ff8
Blowa <blowaxd693@gmail.com>
2017-09-15 23:10:42 +0200

    [ADD] KickSession on WebAPI

---
+++ OpenNos.Master.Server/Controllers/SessionController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using OpenNos.Master.Library.Client;
+
+namespace OpenNos.Master.Server.Controllers
+{
+    public class SessionController : ApiController
+    {
+        // GET api/stats 
+        public void Delete(long accountId)
+        {
+            CommunicationServiceClient.Instance.KickSession(accountId, null);
+        }
+    }
+}
