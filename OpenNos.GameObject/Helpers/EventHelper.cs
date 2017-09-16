6eec59861462e27312c1536de6e4c79276da0f2a
Blowa <blowaxd693@gmail.com>
2017-09-16 19:25:15 +0200

    [FIX] SpawnMonsters Event

--- OpenNos.GameObject/Helpers/EventHelper.cs
+++ OpenNos.GameObject/Helpers/EventHelper.cs
@@ -473,7 +473,7 @@ namespace OpenNos.GameObject.Helpers
                     break;
 
                 case EventActionType.SPAWNMONSTERS:
-                    evt.MapInstance.SummonMonsters((ConcurrentBag<MonsterToSummon>)evt.Parameter);
+                    evt.MapInstance.SummonMonsters(((ConcurrentBag<MonsterToSummon>)evt.Parameter).ToList());
                     break;
                 case EventActionType.REFRESHRAIDGOAL:
                     ClientSession cl = evt.MapInstance.Sessions.FirstOrDefault();
