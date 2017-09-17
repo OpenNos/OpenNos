2d503cd3fbc6c2f79225ef7203f320c8c2774afa
Blowa <blowaxd693@gmail.com>
2017-09-17 18:50:58 +0200

    [REWORK] Reworked Mail system

---
+++ OpenNos.Master.Server/Controllers/MailsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using OpenNos.Master.Library.Client;
+using OpenNos.Domain;
+
+namespace OpenNos.Master.Server.Controllers
+{
+    public class MailsController : ApiController
+    {
+        // GET /stats 
+        public void Get(long accountId)
+        {
+            CommunicationServiceClient.Instance.UpdateMails(accountId);
+        }
+    }
+}
