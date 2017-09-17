b0091b97fb52fb2d9061e5187fab9bbc1ef54c05
0Lucifer0 <erwan.joly@gmail.com>
2017-09-17 16:14:27 +0200

    continue webapi

---
+++ OpenNos.Master.Server/AuthorizationServerProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using OpenNos.Core;
+using OpenNos.DAL;
+using OpenNos.Data;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OpenNos.Master.Server
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            AccountDTO account = DAOFactory.AccountDAO.LoadByName(context.UserName);
+
+
+            if (account != null && account.Password.ToLower().Equals(EncryptionBase.Sha512(context.Password)))
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("sub", context.UserName));
+                identity.AddClaim(new Claim("role", account.Authority.ToString()));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+
+            }
+        }
+    }
+}
